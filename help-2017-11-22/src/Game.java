e74eacd592aa6201908c61a846e0e6b32b793826
Keith Leonard Mannock <keith@dcs.bbk.ac.uk>
2017-11-22 16:31:49 +0000

    Game start

---
+++ help-2017-11-22/src/Game.java
@@ -0,0 +1,8 @@
+/**
+ * Created by keith on 22/11/2017.
+ */
+public class Game {
+
+    public void createPlayers() {
+    }
+}
---
+++ help-2017-11-22/src/MyMain.java
@@ -0,0 +1,10 @@
+/**
+ * Created by keith on 22/11/2017.
+ */
+public class MyMain {
+    public static void main(String[] args) {
+        Game g = new Game();
+        g.createPlayers();
+    }
+
+}
