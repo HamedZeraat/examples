59870e2fdba0433f8de73d283209b640b40a5d2b
Keith Leonard Mannock <keith@dcs.bbk.ac.uk>
2017-12-04 17:15:20 +0000

    bits and pieces

---
+++ session-eight/src/comparable/ThingTest.java
@@ -0,0 +1,13 @@
+package comparable;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ThingTest {
+
+    @Test
+    public void mytest(){
+        Thing t = new Thing();
+    }
+}
