bd45d387242ccaea194860d912765a9ba7e9f0db
Keith Mannock <keith@dcs.bbk.ac.uk>
2018-01-21 21:19:18 +0000

    reorg

---
+++ session-three/src/fibiter/Fibonacci 2.java
@@ -0,0 +1,4 @@
+package fibiter;
+
+public class Fibonacci {
+}
---
+++ session-three/src/worksheet/Book.java
@@ -0,0 +1,31 @@
+package worksheet;
+
+/**
+ * Create a class for books. Books have authors and titles.
+ * The class should implement getters for both author name and title,
+ * but these must be set at construction time and never be modified
+ * afterwards. (Reminder: An object whose fields cannot be changed after construction is
+ * called an immutable object.)
+ */
+public class Book {
+    private String authorName;
+    private String title;
+
+    /**
+     *
+     * @param authorName
+     * @param bookTitle
+     */
+    public Book(String authorName, String bookTitle){
+        this.authorName = authorName;
+        title = bookTitle;
+    }
+
+    public String getAuthorName(){
+        return authorName;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+}
