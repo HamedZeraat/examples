60275c9eddbcd3132ea69b9edd0c7ba9911cf149
Keith L Mannock <keith@dcs.bbk.ac.uk>
2018-01-29 19:55:05 +0000

    Added examples from class on 29th Jan @ BMA

---
+++ session-four/src/Example.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example {
+	public static void main(String[] args){
+		List<String> ls = new ArrayList<>();
+		// Separation of the "spec" from the "impl"
+	}
+}
